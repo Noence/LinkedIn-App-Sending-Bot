193d9cefa0d2565e0169790fbfb9a0e42834a0a1
Noence <85343779+Noence@users.noreply.github.com>
2021-06-10 13:32:54 +0000

    Fix & Improvement: Apply to ALL the jobs
    Fixed the issue where the script would click on a link that would redirect it towards the job-post page. Now clicks the image of the post to avoid being redirected.
    
    Implemented the ability to go to the next page. Code will go through all of the available pages and apply to all the jobs.

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from selenium.webdriver.common.keys import Keys
 import time
 from selenium.common.exceptions import NoSuchElementException
 
-EMAIL = "example@email.com"
-PASSWORD = "password!"
-PHONE_NUMBER = "0000000000"
-REGION = "us"
-
-chrome_driver_path = r"C:\Users\DSDLn\Documents\Dev\chromedriver.exe"
+EMAIL = YOUR LINKEDIN EMAIL
+PASSWORD = YOUR LINKEDIN PASSWORD
+PHONE_NUMBER = YOUR PHONE NUMBER 
+PHONE_REGION = YOUR PHONE REGION (us, fr, en, ...)
+JOB_NAME = YOUR JOB NAME
+REGION = YOUR REGION
+chrome_driver_path = YOUR CHROME DRIVER PATH
 driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
-driver.get("https://www.linkedin.com/jobs/search/?currentJobId=2566892130&f_AL=true&geoId=91000000&keywords=aerospace%20engineer%20intern&location=European%20Union")
+driver.get("https://www.linkedin.com/jobs/search/")
 
 # Sign in code
 time.sleep(1)
@@ -25,16 +26,16 @@ password.send_keys(PASSWORD)
 sign_in = driver.find_element_by_css_selector(".login__form_action_container button")
 sign_in.click()
 
-# Add if Linkedin asks you to make sure you're not a bot so you have time to complete the Captcha
-# time.sleep(30)
+# Add if Linkedin asks you to make sure you're not a bot
+time.sleep(20)
 
 # Searches for the job of your choosing
 search_box = driver.find_element_by_css_selector("#ember47 input")
 search_box.clear()
-search_box.send_keys("space intern")
+search_box.send_keys(JOB_NAME)
 country_box = driver.find_element_by_css_selector("#ember49 input")
 country_box.clear()
-country_box.send_keys("European Union")
+country_box.send_keys(REGION)
 country_box.send_keys(Keys.ENTER)
 time.sleep(1)
 easy_apply_button = driver.find_element_by_css_selector("div .search-reusables__filter-binary-toggle button")
@@ -43,46 +44,74 @@ if easy_apply_button.get_attribute("aria-checked"):
 else:
     easy_apply_button.click()
 time.sleep(1)
-jobs = driver.find_elements_by_class_name("jobs-search-results__list-item")
-print(len(jobs))
 
-for job in jobs:
-    job.click()
-    time.sleep(1)
 
-    # Get rid of all pop-ups that could break the code
-    if driver.find_elements_by_css_selector("div.artdeco-modal.artdeco-modal--layer-default.post-apply-modal") != []:
-        driver.find_element_by_css_selector("button.artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view").click()
+def send_applications():
+    jobs = driver.find_elements_by_class_name("jobs-search-results__list-item")
 
-    if driver.find_elements_by_css_selector("div.artdeco-modal.artdeco-modal--layer-default[role='dialog']") != []:
-        driver.find_element_by_css_selector("button artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view").click()
+    for job in jobs:
+        job.find_element_by_css_selector("img").click()
+        time.sleep(1)
 
-    if driver.find_elements_by_css_selector("li.artdeco-toast-item.artdeco-toast-item--visible.ember-view[data-test-artdeco-toast-item-type='success']") != []:
-        driver.find_element_by_css_selector("button.artdeco-toast-item__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--1.artdeco-button--tertiary.ember-view").click()
+        # App sender
+        try:
+            apply_now_button = driver.find_element_by_css_selector("div.jobs-apply-button--top-card button")
+            apply_now_button.click()
+            apply_now_button = driver.find_element_by_css_selector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+            # Skips jobs if you can't immediately send the application
+            if apply_now_button.text.lower() == "next":
+                close_button = driver.find_element_by_css_selector(".artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view")
+                close_button.click()
+                time.sleep(0.5)
+                discard = driver.find_element_by_css_selector("button.artdeco-modal__confirm-dialog-btn.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+                discard.click()
+            else:
+                # Changes phone number country code
+                country_code = driver.find_element_by_css_selector(f"div.fb-dropdown select option[value='urn:li:country:{PHONE_REGION}']").click()
+                phone_number_text_box = driver.find_element_by_css_selector("div.fb-single-line-text div.display-flex input")
+                # Inputs your phone number
+                phone_number_text_box.clear()
+                phone_number_text_box.send_keys(PHONE_NUMBER)
+                # Submit app
+                submit_button = driver.find_element_by_css_selector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
+                submit_button.click()
+        except NoSuchElementException:
+            # Skips if app has already been sent/not taking apps anymore
+            # print(f"The Job {job.text} is too complex to be automated")
+            continue
+        time.sleep(1)
 
-    # App sender
-    try:
-        apply_now_button = driver.find_element_by_css_selector("div.jobs-apply-button--top-card button")
-        apply_now_button.click()
-        apply_now_button = driver.find_element_by_css_selector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
-        # Skips jobs if you can't immediately send the application
-        if apply_now_button.text.lower() == "next":
-            close_button = driver.find_element_by_css_selector(".artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view")
-            close_button.click()
-            time.sleep(0.5)
-            discard = driver.find_element_by_css_selector("button.artdeco-modal__confirm-dialog-btn.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
-            discard.click()
-        else:
-            # Changes phone number country code
-            country_code = driver.find_element_by_css_selector(f"div.fb-dropdown select option[value='urn:li:country:{REGION}']").click()
-            phone_number_text_box = driver.find_element_by_css_selector("div.fb-single-line-text div.display-flex input")
-            # Inputs your phone number
-            phone_number_text_box.clear()
-            phone_number_text_box.send_keys(PHONE_NUMBER)
-            # Submit app
-            submit_button = driver.find_element_by_css_selector("button.artdeco-button.artdeco-button--2.artdeco-button--primary.ember-view")
-            submit_button.click()
-    except NoSuchElementException:
-        # Skips if app has already been sent/not taking apps anymore
-        # print(f"The Job {job.text} is too complex to be automated")
-        continue
+        # Get rid of all pop-ups that could break the code
+        if driver.find_elements_by_css_selector("div.artdeco-modal.artdeco-modal--layer-default.post-apply-modal") != []:
+            driver.find_element_by_css_selector("button.artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view").click()
+
+        if driver.find_elements_by_css_selector("div.artdeco-modal.artdeco-modal--layer-default[role='dialog']") != []:
+            driver.find_element_by_css_selector("button artdeco-modal__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--2.artdeco-button--tertiary.ember-view").click()
+
+        if driver.find_elements_by_css_selector("li.artdeco-toast-item.artdeco-toast-item--visible.ember-view[data-test-artdeco-toast-item-type='success']") != []:
+            driver.find_element_by_css_selector("button.artdeco-toast-item__dismiss.artdeco-button.artdeco-button--circle.artdeco-button--muted.artdeco-button--1.artdeco-button--tertiary.ember-view").click()
+
+
+send_applications()
+new_section = False
+pages = driver.find_elements_by_css_selector("li.artdeco-pagination__indicator.artdeco-pagination__indicator--number.ember-view")
+number_pages = int(pages[-1].find_element_by_css_selector("span").text)
+print(number_pages)
+if number_pages > 1:
+    for page_number in range(2, number_pages+1):
+        time.sleep(1)
+        new_pages = driver.find_elements_by_css_selector("li.artdeco-pagination__indicator.artdeco-pagination__indicator--number.ember-view")
+        for page in new_pages:
+            if page.find_element_by_css_selector("span").text == str(page_number):
+                page.click()
+                new_section = False
+                break
+            else:
+                new_section = True
+        if new_section:
+            for page in new_pages:
+                if page.find_element_by_css_selector("span").text == "…":
+                    page.click()
+                    new_section = False
+                    break
+        send_applications()
